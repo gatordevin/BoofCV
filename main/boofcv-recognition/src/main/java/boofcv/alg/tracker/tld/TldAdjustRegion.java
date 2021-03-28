f3587b77a701242df9365a4fc7aeecf8b1384df2
Peter Abeles <peter.abeles@gmail.com>
2021-03-27 21:45:15 -0700

    created `boofcv-reconstruction` submodule - This is where `sfm` and `recognition` meet - Created to avoid a cyclical dependency that was forming
    Also described more clearly what several of the submodules are supposed to focus on.

--- main/boofcv-recognition/src/main/java/boofcv/alg/tracker/tld/TldAdjustRegion.java
+++ main/boofcv-recognition/src/main/java/boofcv/alg/tracker/tld/TldAdjustRegion.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, Peter Abeles. All Rights Reserved.
+ * Copyright (c) 2021, Peter Abeles. All Rights Reserved.
  *
  * This file is part of BoofCV (http://boofcv.org).
  *
@@ -18,11 +18,11 @@
 
 package boofcv.alg.tracker.tld;
 
-import boofcv.alg.sfm.robust.DistanceScaleTranslate2DSq;
-import boofcv.alg.sfm.robust.GenerateScaleTranslate2D;
-import boofcv.alg.sfm.robust.ModelManagerScaleTranslate2D;
+import boofcv.alg.geo.robust.DistanceScaleTranslate2DSq;
+import boofcv.alg.geo.robust.GenerateScaleTranslate2D;
+import boofcv.alg.geo.robust.ModelManagerScaleTranslate2D;
 import boofcv.struct.geo.AssociatedPair;
-import boofcv.struct.sfm.ScaleTranslate2D;
+import boofcv.struct.geo.ScaleTranslate2D;
 import georegression.struct.shapes.Rectangle2D_F64;
 import org.ddogleg.fitting.modelset.DistanceFromModel;
 import org.ddogleg.fitting.modelset.ModelGenerator;
